679b01a0f6c671991f8093eb87982cda5244059d
valfvo <49768521+valfvo@users.noreply.github.com>
2020-12-30 15:08:59 +0100

    Update RuleSet ctor

---
+++ src/RuleSet.cpp
@@ -0,0 +1,10 @@
+#include "RuleSet.hpp"
+
+namespace LQ {
+
+RuleSet::RuleSet(std::string_view classList)
+    : classList_(classList)
+{
+}
+
+} // namespace LQ
--- src/RuleSet.hpp
+++ src/RuleSet.hpp
@@ -7,8 +7,11 @@ namespace LQ {
 class RuleSet {
 public:
     RuleSet() {}
-    RuleSet(std::string_view classList) {}
+    RuleSet(std::string_view classList);
     virtual ~RuleSet() {};
+
+private:
+    std::string_view classList_;
 };
 
 } // namespace LQ
